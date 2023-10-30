2d9d44e0e7eaa508aebd764a611b16f3f59fdf79
Blackmonkcr <aaron.navarro@utec.edu.pe>
2023-10-30 17:17:29 -0500

    Copy example

--- hck4/src/App.js
+++ hck4/src/App.js
@@ -1,42 +1,93 @@
-import 'devextreme/dist/css/dx.common.css';
-import './themes/generated/theme.base.css';
-import './themes/generated/theme.additional.css';
 import React from 'react';
-import { HashRouter as Router } from 'react-router-dom';
-import './dx-styles.scss';
-import LoadPanel from 'devextreme-react/load-panel';
-import { NavigationProvider } from './contexts/navigation';
-import { AuthProvider, useAuth } from './contexts/auth';
-import { useScreenSizeClass } from './utils/media-query';
-import Content from './Content';
-import UnauthenticatedContent from './UnauthenticatedContent';
 
-function App() {
-  const { user, loading } = useAuth();
+import ODataStore from 'devextreme/data/odata/store';
 
-  if (loading) {
-    return <LoadPanel visible={true} />;
+import DataGrid, {
+  Column,
+  Grouping,
+  GroupPanel,
+  Pager,
+  Paging,
+  SearchPanel,
+} from 'devextreme-react/data-grid';
+
+import DiscountCell from './DiscountCell.js';
+
+const pageSizes = [10, 25, 50, 100];
+
+const dataSourceOptions = {
+  store: new ODataStore({
+    url: 'https://js.devexpress.com/Demos/SalesViewer/odata/DaySaleDtoes',
+    key: 'Id',
+    beforeSend(request) {
+      const year = new Date().getFullYear() - 1;
+      request.params.startDate = `${year}-05-10`;
+      request.params.endDate = `${year}-5-15`;
+    },
+  }),
+};
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      collapsed: false,
+    };
+    this.onContentReady = this.onContentReady.bind(this);
   }
 
-  if (user) {
-    return <Content />;
+  render() {
+    return (
+      <DataGrid
+        dataSource={dataSourceOptions}
+        allowColumnReordering={true}
+        rowAlternationEnabled={true}
+        showBorders={true}
+        width="100%"
+        onContentReady={this.onContentReady}
+      >
+        <GroupPanel visible={true} />
+        <SearchPanel visible={true} highlightCaseSensitive={true} />
+        <Grouping autoExpandAll={false} />
+
+        <Column dataField="Product" groupIndex={0} />
+        <Column
+          dataField="Amount"
+          caption="Sale Amount"
+          dataType="number"
+          format="currency"
+          alignment="right"
+        />
+        <Column
+          dataField="Discount"
+          caption="Discount %"
+          dataType="number"
+          format="percent"
+          alignment="right"
+          allowGrouping={false}
+          cellRender={DiscountCell}
+          cssClass="bullet"
+        />
+        <Column dataField="SaleDate" dataType="date" />
+        <Column dataField="Region" dataType="string" />
+        <Column dataField="Sector" dataType="string" />
+        <Column dataField="Channel" dataType="string" />
+        <Column dataField="Customer" dataType="string" width={150} />
+
+        <Pager allowedPageSizes={pageSizes} showPageSizeSelector={true} />
+        <Paging defaultPageSize={10} />
+      </DataGrid>
+    );
   }
 
-  return <UnauthenticatedContent />;
+  onContentReady(e) {
+    if (!this.state.collapsed) {
+      e.component.expandRow(['EnviroCare']);
+      this.setState({
+        collapsed: true,
+      });
+    }
+  }
 }
 
-export default function Root() {
-  const screenSizeClass = useScreenSizeClass();
-
-  return (
-    <Router>
-      <AuthProvider>
-        <NavigationProvider>
-          <div className={`app ${screenSizeClass}`}>
-            <App />
-          </div>
-        </NavigationProvider>
-      </AuthProvider>
-    </Router>
-  );
-}
+export default App;
